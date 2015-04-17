b857b83923c941c669ff315cc7c9fc1130f5b6fe
Shayan Mohanty <shayan.mohanty@gmail.com>
2015-04-17 11:42:38 -0500

    Fixing project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject hexpacker "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A hexagonal circle packing implementation for canvasing/sampling large geographical areas (specifically optimized for Google/Instagram/Twitter APIs)"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
                  [clj-http "1.0.1"]
