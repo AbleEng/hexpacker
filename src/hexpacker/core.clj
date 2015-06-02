0ca5b2a941b71f9d9b38ba956d7dda09caec8cfd
Shayan Mohanty <shayan.mohanty@gmail.com>
2015-06-02 14:08:34 -0500

    Fixed up -main for remote execution

--- src/hexpacker/core.clj
+++ src/hexpacker/core.clj
@@ -27,14 +27,14 @@
   (let [req-num (count packed-circle-coords)]
     (println (str "Making " req-num " requests to Google, Instagram, and Twitter...")))
   ;;; Make requests & store results (SMALL TEST)
-  (println "Getting Google responses...")
-  (def google-response (pmap get-google-places-data test-list))
+  ;; (println "Getting Google responses...")
+  ;; (def google-response (pmap get-google-places-data test-list))
 
-  (println "Getting Instagram responses...")
-  (map get-instagram-data test-list)
+  ;; (println "Getting Instagram responses...")
+  ;; (map get-instagram-data test-list)
 
-  (println "Getting Twitter responses...")
-  (map get-twitter-data test-list) 
+  ;; (println "Getting Twitter responses...")
+  ;; (map get-twitter-data test-list) 
 
   ;;; Make requests & store results (SCALED TEST)
   ;; (println "Getting Google responses...")
@@ -46,65 +46,72 @@
   ;; (println "Getting Instagram responses...")
   ;; (doall (map get-instagram-data packed-circle-coords))
 
+  ;;; Make requests & store results (CSV TEST)
+  (println "Getting Google responses...")
+  (def google-response (doall (map get-google-places-data packed-circle-coords)))
+
   (println "Transforming results...")
   ;;; Transform responses to more workable states
-  (def google-response-coords (set (flatten (let [response-results (map :results google-response)]
-                                          (for [result response-results]
-                                            (for [sub-result result]
-                                              {:name (:name sub-result)
-                                               :location (:location (:geometry sub-result))}))))))
+  ;; (def google-response-coords (set (flatten (let [response-results (map :results google-response)]
+  ;;                                         (for [result response-results]
+  ;;                                           (for [sub-result result]
+  ;;                                             {:name (:name sub-result)
+  ;;                                              :location (:location (:geometry sub-result))}))))))
 
+  ;; (def twitter-statuses (filter (fn [elem]
+;;                                   (if (and 
+;;                                        (not= nil (:lat (:location elem))) 
+;;                                        (not= nil (:lng (:location elem))))
+;;                                     true
+;;                                     false)) 
+;;                                 (flatten (let [statuses (flatten (map :statuses @twitter-responses))]
+;;                                            (for [status statuses]
+;;                                              (let [[lat lng] (:coordinates (:geo status))]
+;;                                                {:user (:screen_name (:user status))
+;;                                                 :tweet (:text status)
+;;                                                 :location {:lat lat :lng lng}}))))))
+
+;;   (def instagram-media-list
+;;     (map (fn [elem]
+;;            (let [media {:link (:link elem) 
+;;                         :location (:location elem)}]
+;;              media)) (flatten (map :data @instagram-responses))))
+
+
+;; ;;; Combine the data and order the results
+;;   (def combined-data (for [biz google-response-coords]
+;;                        (let [instagram-results (filter #(not (nil? %1)) (for [media instagram-media-list]
+;;                                                                           (let [lat (:latitude (:location media))
+;;                                                                                 lng (:longitude (:location media))
+;;                                                                                 ll {:lat lat :lng lng}
+;;                                                                                 distance (haversine (:location biz) ll)]
+;;                                                                             (if (< distance 0.015)
+;;                                                                               {:distance distance
+;;                                                                                :link (:link media)}))))
+;;                              twitter-results (filter #(not (nil? %1)) (for [status twitter-statuses]
+;;                                                                         (let [lat (:lat (:location status))
+;;                                                                               lng (:lng (:location status))
+;;                                                                               ll {:lat lat :lng lng}
+;;                                                                               distance (haversine (:location biz) ll)]
+;;                                                                           (if (< distance 0.015)
+;;                                                                             {:distance (haversine (:location biz) ll)
+;;                                                                              :tweet (:tweet status)
+;;                                                                              :user (:user status)}))))
+;;                              total-results (into [] (flatten (conj instagram-results twitter-results)))]
+;;                          (conj biz {:results total-results}))))
+
+;;   (def ordered-results (reverse (sort-by #(count (:results %1)) combined-data))) 
+  ;; (pprint ordered-results)
+
+  ;; For Google Places CSV export
   (def google-places-cleaned (set (flatten (let [response-results (map :results google-response)]
                                              (for [result response-results]
                                                (for [sub-result result]
                                                  {:name (:name sub-result)
                                                   :place_id (:place_id sub-result)
                                                   :types (:types sub-result)}))))))
-
-  (def twitter-statuses (filter (fn [elem]
-                                  (if (and 
-                                       (not= nil (:lat (:location elem))) 
-                                       (not= nil (:lng (:location elem))))
-                                    true
-                                    false)) 
-                                (flatten (let [statuses (flatten (map :statuses @twitter-responses))]
-                                           (for [status statuses]
-                                             (let [[lat lng] (:coordinates (:geo status))]
-                                               {:user (:screen_name (:user status))
-                                                :tweet (:text status)
-                                                :location {:lat lat :lng lng}}))))))
-
-  (def instagram-media-list
-    (map (fn [elem]
-           (let [media {:link (:link elem) 
-                        :location (:location elem)}]
-             media)) (flatten (map :data @instagram-responses))))
-
-
-;;; Combine the data and order the results
-  (def combined-data (for [biz google-response-coords]
-                       (let [instagram-results (filter #(not (nil? %1)) (for [media instagram-media-list]
-                                                                          (let [lat (:latitude (:location media))
-                                                                                lng (:longitude (:location media))
-                                                                                ll {:lat lat :lng lng}
-                                                                                distance (haversine (:location biz) ll)]
-                                                                            (if (< distance 0.015)
-                                                                              {:distance distance
-                                                                               :link (:link media)}))))
-                             twitter-results (filter #(not (nil? %1)) (for [status twitter-statuses]
-                                                                        (let [lat (:lat (:location status))
-                                                                              lng (:lng (:location status))
-                                                                              ll {:lat lat :lng lng}
-                                                                              distance (haversine (:location biz) ll)]
-                                                                          (if (< distance 0.015)
-                                                                            {:distance (haversine (:location biz) ll)
-                                                                             :tweet (:tweet status)
-                                                                             :user (:user status)}))))
-                             total-results (into [] (flatten (conj instagram-results twitter-results)))]
-                         (conj biz {:results total-results}))))
-
-  (def ordered-results (reverse (sort-by #(count (:results %1)) combined-data))) 
-  ;; (pprint ordered-results)
+  
+  (write-csv "/tmp/results.csv" google-places-cleaned)
 
   ;; For debugging
   (def test-coords 
